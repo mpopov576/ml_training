33b98c4296625f9278d36dee40923c3f4439a8ef
mpopov576 <miroslavpopov576@gmail.com>
2025-11-05 21:12:14 +0200

    Update metrics.py
    def accuracy_score()

--- ml_lib/metrics.py
+++ ml_lib/metrics.py
@@ -1,1 +1,14 @@
+import numpy as np
 
+def accuracy_score(y_true, y_pred, normalize=True):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+
+    if y_true.shape != y_pred.shape:
+        raise ValueError('y_true and y_shape must have the same shape')
+
+    correct = np.sum(y_pred == y_true)
+
+    if normalize:
+        return correct / len(y_true)
+    else:
+        return correct
