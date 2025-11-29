99c4f66c98deaa851ae33cecdbc96db57f9b70f5
mpopov576 <miroslavpopov576@gmail.com>
2025-11-30 00:55:29 +0200

    Update metrics.py
    added r2_score function

--- ml_lib/metrics.py
+++ ml_lib/metrics.py
@@ -31,3 +31,10 @@ def manhattan_distance(x, y):
         raise ValueError("shape of x and y must be equal")
 
     return np.sum(np.abs(p1 - p2))
+
+def r2_score(y_true, y_pred):
+    y_mean = np.mean(y_true)
+
+    ss_tot = np.sum((y_true - y_mean)**2)
+    ss_res = np.sum((y_true - y_pred)**2)
+    return 1 - ss_res / ss_tot
